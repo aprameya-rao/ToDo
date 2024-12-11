6dfd1a79222202ea664d928048e4b49112c95850
aprameya-rao <rao.aprameya1205@gmail.com>
2024-12-11 19:44:25 +0530

    11/12/24.4

--- script.js
+++ script.js
@@ -3,6 +3,53 @@
 let todo = JSON.parse(localStorage.getItem("todo")) || [];
 
 const todoInput= document.getElementById("todoInput");
+const todoList= document.getElementById("todoList");
+const todoCount= document.getElementById("todoCount");
+const addButton= document.querySelector(".btn");
+const deleteButton=document.getElementById("deleteButton")
 
+
+//intialise
+
+document.addEventListener("DOMContentLoaded",function(){
+    addButton.addEventListener("click" ,addTask); 
+    todoInput.addEventListener("keydown",function(event)
+    {
+        if(event.key==="Enter"){
+            event.preventDefault();
+            addTask();
+        }
+    });
+    deleteButton.addEventListener("click",deleteAllTasks);
+    displayTasks();
+});
+
+function addTask(){
+    const newTask=todoInput.value.trim();
+    if(newTask !== "" )
+        {
+        todo.push(
+            {
+            text: newTask,
+            disabled: false,
+        });
+        saveToLocalStorage();
+        todoInput.value="";
+        displayTasks();
+    }
+
+}
+
+function deleteAllTasks(){
+
+}
+
+function displayTasks(){
+
+}
+
+function saveToLocalStorage(){
+    localStorage.setItem("todo",JSON.stringify(todo));
+}
 
 
